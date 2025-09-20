18f64f823ec7702f9966417436b6150141cdadde
ShmuelBarazani <tropikan1@gmail.com>
2025-09-20 20:51:50 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,5 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-// אם יש לך קובץ CSS כללי, השאר את הייבוא הזה/התאם את הנתיב:
-import "./styles/styles.css";
-
-const el = document.getElementById("root");
-createRoot(el).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
