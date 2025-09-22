bb55b9264506f417914b72bbee7e22b2188acf44
ShmuelBarazani <tropikan1@gmail.com>
2025-09-22 12:45:54 +0300

    Delete src/pages/Teams.jsx

--- src/pages/Teams.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/Teams.jsx
-// עטיפה שמרנדרת את TeamMaker — כך שגם אם ניווט פונה ל-"Teams" נטען את המסך הנכון
-
-import React from "react";
-import TeamMaker from "./TeamMaker";
-
-export default function TeamsPage(props) {
-  return <TeamMaker {...props} />;
-}
