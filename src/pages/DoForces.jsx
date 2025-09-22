c8220c8a145c415e5140ec852c127adada85d386
ShmuelBarazani <tropikan1@gmail.com>
2025-09-22 12:45:36 +0300

    Delete src/pages/DoForces.jsx

--- src/pages/DoForces.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/DoForces.jsx
-// עטיפה שמרנדרת תמיד את TeamMaker המעודכן (ללא כפתור איפוס + איזון קשיח + PrintView חדש)
-
-import React from "react";
-import TeamMaker from "./TeamMaker";
-
-export default function DoForcesPage(props) {
-  return <TeamMaker {...props} />;
-}
