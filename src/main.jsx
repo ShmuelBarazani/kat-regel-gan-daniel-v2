465e6f14fc190b81b440eac73b36e2ac470dd182
ShmuelBarazani <tropikan1@gmail.com>
2025-09-22 09:02:46 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
+// src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
+// כאן ייבוא ה-CSS במקום אחד בלבד
+import "./styles.css"; 
+// אם הקובץ שלך נמצא בתוך תיקייה src/styles/styles.css:
+// שנה לשורה: import "./styles/styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
