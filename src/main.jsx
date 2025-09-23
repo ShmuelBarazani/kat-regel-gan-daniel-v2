bee3743392f1a085907aeafae6aa41646ba8eed2
ShmuelBarazani <tropikan1@gmail.com>
2025-09-23 09:42:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { AppProvider } from "@/store/playerStorage.jsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
